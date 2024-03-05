3ac98c2af3bcb894104acf3440e97b974429479c
jbarus <57799873+jbarus@users.noreply.github.com>
2024-03-05 17:05:36 +0100

    Added basic structure for backend

---
+++ backend/src/test/java/com/example/boil/BoilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
