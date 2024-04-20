85c91841513b436cbb34fd59e61872c4eead125b
misba <misbartszek@gmail.com>
2024-04-20 20:15:27 +0200

    Uszczelnienie dodawania akcji

--- frontend/src/features/CPM/CpmForm.tsx
+++ frontend/src/features/CPM/CpmForm.tsx
@@ -23,7 +23,7 @@ export const CpmForm = () => {
     const [formData, setFormData] = useState<{ name: string; time: number | string; start: string | null; end: string | null; }[]>([]);
     const [showDiagram, setShowDiagram] = useState(true);
     const [editableIndex, setEditableIndex] = useState<number | null>(null);
-    
+
     const [showAlert, setShowAlert] = useState(false);
 
 
@@ -56,7 +56,7 @@ export const CpmForm = () => {
     // funkcja wywołująca się po zmianie wartości w polu liczbowym
     const handleNumberInputChange = (newValue: number | string) => {
         setActionTime(newValue);
-        console.log('Nowa wartość siemanko:', actionTime);
+        // console.log('Nowa wartość siemanko:', actionTime);
     };
 
     const eventOnClick = () => {
@@ -81,54 +81,67 @@ export const CpmForm = () => {
 
     const findEventByName = (eventName: string): Event => {
         let myObject: Event | undefined = eventUse.find(event => event.name === eventName);
-        let eventbyname=myObject as Event;
-        return  eventbyname;
+        let eventbyname = myObject as Event;
+        return eventbyname;
     };
 
     const eventOnClick2 = () => {
-        //console.log("Button clicked with event:", eventName, "and action:", actionName);
+        console.log("Button clicked action:", actionName, "start: ", actionStart, "end: ", actionEnd);
 
-        if (eventName && actionName && nameStart.includes(eventName) && nameEnd.includes(eventName)) {
+        if (eventName && actionName && nameStart.includes(eventName) && nameEnd.includes(eventName) && actionStart !== null && actionEnd !== null && actionStart < actionEnd) {
+
+
             const actionEndString: string = actionEnd ?? "";
             const actionStartString: string = actionStart ?? "";
             const timeNumber: number = typeof actionTime === "string" ? parseFloat(actionTime) : actionTime as number;
 
 
-            let tmpActivity:Activity= new Activity(actionName,timeNumber,actionStartString,actionEndString )
+            let tmpActivity: Activity = new Activity(actionName, timeNumber, actionStartString, actionEndString)
 
-            setactivityUse(prevActivity => [...prevActivity, tmpActivity]);
-            const newData = {
-                name: actionName,
-                time: actionTime,
+            const isActivityExist = activityUse.find(activity =>
+                activity.name === actionName || (activity.startId === actionStart && activity.endId === actionEnd)
+            );
+            if (isActivityExist) {
 
-                start: actionStart,
-                end: actionEnd
-            };
-            setFormData(prevData => [...prevData, newData]);
-           
+                console.log("Ta aktywność już istnieje!");
+            } else {
+                //
+                setactivityUse(prevActivity => [...prevActivity, tmpActivity]);
+                const newData = {
+                    name: actionName,
+                    time: actionTime,
+
+                    start: actionStart,
+                    end: actionEnd
+                };
+                setFormData(prevData => [...prevData, newData]);
+            }
+
+
+
 
         }
-        console.log("hejka tu lenka");
+
         console.log(activityUse);
 
     };
 
     const handleDelete = (indexToDelete: number) => {
         const updatedFormData = [...formData];
-        updatedFormData.splice(indexToDelete, 1); 
+        updatedFormData.splice(indexToDelete, 1);
         setFormData(updatedFormData);
-    
-   
+
+
         const updatedActivityUse = [...activityUse];
         updatedActivityUse.splice(indexToDelete, 1);
         setactivityUse(updatedActivityUse);
     };
-    
+
     const handleEdit = (indexToEdit: number | undefined) => {
         if (indexToEdit !== undefined) {
-            setEditableIndex(indexToEdit); 
+            setEditableIndex(indexToEdit);
         } else {
-            setEditableIndex(null); 
+            setEditableIndex(null);
             const updatedActivityUse = formData.map(data => {
                 return new Activity(data.name, parseFloat(data.time as string), data.start as string, data.end as string);
             });
@@ -139,47 +152,47 @@ export const CpmForm = () => {
 
     const diagram_click = async () => {
         console.log("Fetching data...");
-        await fetchData(); 
+        await fetchData();
         console.log("Data fetched successfully");
-        setShowDiagram(true); 
+        setShowDiagram(true);
         const cy = graph(activityUse, eventUseGet);
     };
 
     const Oblicz = async () => {
         console.log("Diagram dupa clicked");
         await fetchData();
-       
+
     };
 
 
-    const fetchData = async ()=>{
+    const fetchData = async () => {
         try {
-            console.log( JSON.stringify(activityUse));
-            const response = await fetch(BASE_API_URL + 'v1/cpm/activity',{
-                
+            console.log(JSON.stringify(activityUse));
+            const response = await fetch(BASE_API_URL + 'v1/cpm/activity', {
+
                 method: 'POST',
                 headers: {
                     'Access-Control-Allow-Origin': '*',
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify(activityUse),
+                },
+                body: JSON.stringify(activityUse),
             }
-       
+
             );
             if (response.ok) {
                 const responseBody = await response.json(); // Parse response body as JSON
                 console.log('Success');
-            let tmp:Event[]=responseBody
+                let tmp: Event[] = responseBody
                 setEventUseGet(tmp);
                 console.log(tmp);
-              } else {
+            } else {
                 console.error('Error ');
-              }
-                   }
-                 catch (error) {
-                    console.error('Error:', error);
-                  }
+            }
+        }
+        catch (error) {
+            console.error('Error:', error);
+        }
     }
 
     return (
@@ -189,6 +202,7 @@ export const CpmForm = () => {
                 <NumberInput
                     onChange={handleInputChangeEvent}
                     placeholder="Nazwa"
+                    allowNegative={false}
                 />
 
 
@@ -218,11 +232,12 @@ export const CpmForm = () => {
                     placeholder="Nazwa"
                     onChange={handleInputChangeAction}
                     style={{ width: '10%' }}
+
                 />
 
                 {/* Text field z czasem wykonania */}
                 <NumberInput
-
+                    allowNegative={false}
                     onChange={handleNumberInputChange}
                     placeholder="Wprowadz czas wykonania"
                 />
@@ -249,39 +264,39 @@ export const CpmForm = () => {
                 variant="gradient"
                 gradient={{ from: 'red', to: 'blue', deg: 263 }}
                 onClick={diagram_click}
-            >   Diagram  
+            >   Diagram
             </Button>
 
             <Button
                 variant="gradient"
                 gradient={{ from: 'red', to: 'blue', deg: 263 }}
                 onClick={Oblicz}
-                style={{ marginLeft: '10px' }} 
-            >   Oblicz  
+                style={{ marginLeft: '10px' }}
+            >   Oblicz
             </Button>
 
             <div style={{ overflowX: 'auto', maxHeight: '300px', overflowY: 'auto' }}>
-            <table style={{ borderCollapse: 'collapse', width: '100%', borderBottom: '4px solid black' }}>
-                <thead>
-                    <tr style={{ backgroundColor: '#f8f9fa', borderBottom: '2px solid #dee2e6' }}>
-                        <th style={{ padding: '10px', textAlign: 'left', borderBottom: '1px solid #dee2e6' }}>Nazwa</th>
-                        <th style={{ padding: '10px', textAlign: 'left', borderBottom: '1px solid #dee2e6' }}>Czas</th>
-                        <th style={{ padding: '10px', textAlign: 'left', borderBottom: '1px solid #dee2e6' }}>Zdarzenie początkowe</th>
-                        <th style={{ padding: '10px', textAlign: 'left', borderBottom: '1px solid #dee2e6' }}>Zdarzenie końcowe</th>
-                    </tr>
-                </thead>
-                <tbody>
+                <table style={{ borderCollapse: 'collapse', width: '100%', borderBottom: '4px solid black' }}>
+                    <thead>
+                        <tr style={{ backgroundColor: '#f8f9fa', borderBottom: '2px solid #dee2e6' }}>
+                            <th style={{ padding: '10px', textAlign: 'left', borderBottom: '1px solid #dee2e6' }}>Nazwa</th>
+                            <th style={{ padding: '10px', textAlign: 'left', borderBottom: '1px solid #dee2e6' }}>Czas</th>
+                            <th style={{ padding: '10px', textAlign: 'left', borderBottom: '1px solid #dee2e6' }}>Zdarzenie początkowe</th>
+                            <th style={{ padding: '10px', textAlign: 'left', borderBottom: '1px solid #dee2e6' }}>Zdarzenie końcowe</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                         {formData.map((data, index) => (
                             <tr key={index} style={{ borderBottom: '1px solid #dee2e6' }}>
                                 <td style={{ padding: '10px' }}>
                                     {editableIndex === index ? (
-                                        <TextInput 
-                                        value={data.name} 
-                                        onChange={(e) => {
-                                            const newData = [...formData];
-                                            newData[index].name = e.target.value;
-                                            setFormData(newData);
-                                        }} 
+                                        <TextInput
+                                            value={data.name}
+                                            onChange={(e) => {
+                                                const newData = [...formData];
+                                                newData[index].name = e.target.value;
+                                                setFormData(newData);
+                                            }}
                                         />
                                     ) : (
                                         <span>{data.name}</span>
@@ -289,13 +304,13 @@ export const CpmForm = () => {
                                 </td>
                                 <td style={{ padding: '10px' }}>
                                     {editableIndex === index ? (
-                                        <NumberInput 
-                                        value={data.time} 
-                                        onChange={(value) => {
-                                            const newData = [...formData];
-                                            newData[index].time = value;
-                                            setFormData(newData);
-                                        }} 
+                                        <NumberInput
+                                            value={data.time}
+                                            onChange={(value) => {
+                                                const newData = [...formData];
+                                                newData[index].time = value;
+                                                setFormData(newData);
+                                            }}
                                         />
                                     ) : (
                                         <span>{data.time}</span>
@@ -303,14 +318,14 @@ export const CpmForm = () => {
                                 </td>
                                 <td style={{ padding: '10px' }}>
                                     {editableIndex === index ? (
-                                        <Select 
-                                        value={data.start} 
-                                        data={nameStart.map(name => ({ value: name, label: name }))} 
-                                        onChange={(value) => {
-                                            const newData = [...formData];
-                                            newData[index].start = value;
-                                            setFormData(newData);
-                                        }} 
+                                        <Select
+                                            value={data.start}
+                                            data={nameStart.map(name => ({ value: name, label: name }))}
+                                            onChange={(value) => {
+                                                const newData = [...formData];
+                                                newData[index].start = value;
+                                                setFormData(newData);
+                                            }}
                                         />
                                     ) : (
                                         <span>{data.start}</span>
@@ -318,14 +333,14 @@ export const CpmForm = () => {
                                 </td>
                                 <td style={{ padding: '10px' }}>
                                     {editableIndex === index ? (
-                                        <Select 
-                                        value={data.end} 
-                                        data={nameEnd.map(name => ({ value: name, label: name }))} 
-                                        onChange={(value) => {
-                                            const newData = [...formData];
-                                            newData[index].end = value;
-                                            setFormData(newData);
-                                        }} 
+                                        <Select
+                                            value={data.end}
+                                            data={nameEnd.map(name => ({ value: name, label: name }))}
+                                            onChange={(value) => {
+                                                const newData = [...formData];
+                                                newData[index].end = value;
+                                                setFormData(newData);
+                                            }}
                                         />
                                     ) : (
                                         <span>{data.end}</span>
@@ -344,7 +359,7 @@ export const CpmForm = () => {
                             </tr>
                         ))}
                     </tbody>
-            </table>
+                </table>
             </div>
             {showDiagram && <div id="cy" style={{ width: '100%', height: '400px' }}></div>}
         </div>
